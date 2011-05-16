2529cb12c955ebb5f6b34c6fc59a423abe75234d
Mark Ellis <m@rkellis.com>
2011-05-16 02:15:55 +0000

    tagged 0.005

---
+++ trunk/lib/Data/Validate/Image.pm
@@ -0,0 +1,133 @@
+package Data::Validate::Image;
+use strict;
+use warnings;
+
+our $VERSION = '0.005';
+$VERSION = eval $VERSION;
+
+use Image::Info;
+
+sub new{
+    my $incovant = shift;
+    my $class = ref( $incovant ) || $incovant;
+
+    my $self = {};
+    bless( $self, $class );
+
+    return $self;
+}
+
+sub validate{
+    my ( $self, $file ) = @_;
+
+    my $image_type = Image::Info::image_info($file);
+
+    if ( !defined( $image_type->{'file_ext'} ) ){
+        return undef;
+    }
+
+    my $image_info = {
+        'width' => $image_type->{'width'},
+        'height' => $image_type->{'height'},
+        'size' => (-s $file) / 1024,
+        'mime' => $image_type->{'file_media_type'},
+        'file_ext' => $image_type->{'file_ext'},
+    };
+
+    if ( $self->_convert_installed ){ #test if imagemagic is installed
+        my @frames = `convert -identify '${file}' /dev/null 2> /dev/null`;
+
+        $image_info->{'frames'} = scalar( @frames );
+        $image_info->{'animated'} = ($#frames) ? 1 : 0;
+
+        if ( $? ){
+        # convert returns 0 on success - prolly a corrupt image
+            return undef;
+        }
+    }
+
+    return $image_info;
+}
+
+sub _convert_installed{
+    my ( $self ) = @_;
+
+    my @paths = split( /:|;/, $ENV{PATH} );
+    foreach my $path ( @paths ){
+        if ( -e "${path}/convert" ){
+            return 1;
+        }   
+    }
+
+    return 0;
+}
+
+1;
+
+=head1 NAME
+
+Data::Validate::Image - Validates an image and returns basic info
+
+=head1 IMPORTANT
+
+B<REQUIRES> convert (from imagemagick) to be installed and in the path for animated gif/frame detection
+
+I used convert over PerlMagick because I found PerlMagick to be very unstable.
+
+=head1 SYNOPSIS
+
+    use Data::Validate::Image;
+
+    my $validator = Data::Validate::Image->new();
+    my $image_info = $validator->validate( '/path/to/image' );
+
+    if ( defined( $image_info ) ){
+        #valid image, do things here
+    } else {
+        #invalid image
+    }
+
+=head1 DESCRIPTION
+
+pretty simple image validator class. returns hash of image properties on success, 
+
+undef for invalid images
+
+hash properties are
+
+    'width' => image width,
+    'height' => image height,
+    'size' => image filesize (KB),
+    'mime' => image mime type,
+    'file_ext' => *correct* file extenstion,
+    'frames' => frame count, #requires convert from imagemagic to be installed
+    'animated' => 1 || 0, #requires convert from imagemagic to be installed
+
+=head1 METHODS
+
+=head2 validate
+
+    returns image info or undef for invalid image
+
+=head1 SUPPORT
+
+Bugs should always be submitted via the CPAN bug tracker
+
+For other issues, contact the maintainer
+
+=head1 AUTHORS
+
+n0body E<lt>n0body@thisaintnews.comE<gt>
+
+=head1 SEE ALSO
+
+L<http://thisaintnews.com>, L<Image::Info>
+
+=head1 LICENSE
+
+Copyright (C) 2011 by n0body L<http://thisaintnews.com/>
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ trunk/t/validate_image.t
@@ -0,0 +1,46 @@
+use strict;
+use warnings;
+
+use Test::More;
+use Data::Validate::Image;
+use Cwd 'abs_path';
+use File::Basename;
+
+my $dir = dirname( abs_path( $0 ) ) . "/test_data";
+my @images = <${dir}/images/*>;
+my @fakes = <${dir}/fakes/*>;
+
+my $filelist = {
+    'images' => \@images,
+    'fakes' => \@fakes,
+};
+my $validator = new_ok('Data::Validate::Image');
+
+foreach my $image ( @{$filelist->{'images'}} ){
+    my $image_info = $validator->validate( $image );
+    isnt( $image_info, 0, "${image} is " . $image_info->{'file_ext'} );
+    ok( $image_info->{'width'}, 'width defined ' . $image_info->{'width'} );
+    ok( $image_info->{'height'}, 'height defined ' . $image_info->{'height'} );
+    ok( $image_info->{'size'}, 'size defined ' . $image_info->{'size'} );
+    ok( $image_info->{'file_ext'}, 'mime type defined ' . $image_info->{'file_ext'} );
+    ok( $image_info->{'mime'}, 'file_ext defined ' . $image_info->{'mime'} );
+
+    if ( $validator->_convert_installed ){
+        if ( $image =~ m/^${dir}\/images\/animated(\d+)\.gif$/ ){
+            ok( $image_info->{'animated'}, 'animated gif' ) ;
+            is( $image_info->{'frames'}, $1, 'correct frame count' );
+        }
+
+        if ( $image =~ m/^${dir}\/images\/not_animated.gif$/ ){
+            ok( !$image_info->{'animated'}, 'not animated gif' ) ;
+            is( $image_info->{'frames'}, 1, 'correct frame count' );
+        }
+    }
+}
+
+foreach my $fake ( @{$filelist->{'fakes'}} ){
+    my $image_info = $validator->validate( $fake );
+    is( $image_info, undef, "image_type is undef" );
+}
+
+done_testing();
